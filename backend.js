ee2378af60e28b85b1b83ec175333804334e5b75
HoneyEmy <melrillyemillyrs@gmail.com>
2026-04-23 20:58:42 -0300

    Cria backend com função de cadastro de cursos

---
+++ backend.js
@@ -0,0 +1,19 @@
+let cursos = [] ;
+function adicionarCurso(nome, cargaHoraria, descricao) {
+    const curso = {
+        nome,
+        cargaHoraria,
+        descricao
+    };
+    cursos.push(curso);
+    console.log("Curso adicionado!");
+}
+funtion listarCursos() {
+    console.log("Lista de cursos da Disponibiliza Curso:");
+    cursos.forEach((curso, index) => {
+        console.log('${index + 1} - ${curso.nome} (${curso.cargaHoraria}h)');
+    });
+}
+adicionarCurso("Limpeza de Pele", 20, "Curso completo de cuidados com a pele");
+adicionarCurso("Designer de Sobrancelha", 15, "Técnicas profissionais de sobrancelha");
+listarCursos();
